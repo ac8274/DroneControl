851838d3f63a149379cbda660f22fde4f47db31f
Cats_army <ariechera@gmail.com>
2024-12-19 09:20:52 +0200

    started second activity

---
+++ app/src/main/java/com/example/dronecontrol/Selection_Screen.java
@@ -0,0 +1,16 @@
+package com.example.dronecontrol;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class Selection_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selection_screen);
+
+    }
+}
