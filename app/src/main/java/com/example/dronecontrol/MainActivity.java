6ae500e3e0b8bd6ac70530d3c1818564737624e5
Magshimim <ac8274@bs.amalnet.k12.il>
2025-01-05 15:40:06 +0200

    changed the name of the file created and the description of it. nothing big

--- app/src/main/java/com/example/dronecontrol/MainActivity.java
+++ app/src/main/java/com/example/dronecontrol/MainActivity.java
@@ -99,7 +99,7 @@ public class MainActivity extends AppCompatActivity {
                 });
     }
 //added
-    //
+    //cg
     public void onSuccess()
     {
         UserUid.user_uid = auth.getCurrentUser().getUid();
